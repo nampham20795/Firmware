e578a55d5aa9d74e4eb5d482f552217a93e28d4a
nampham20795 <nampham20795@gmail.com>
2017-12-07 19:46:28 +0700

    Deselect vehicle control files in cmake

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -112,14 +112,14 @@ set(config_module_list
 
 	#
 	# Vehicle Control
-	#
-	modules/fw_att_control
-	modules/fw_pos_control_l1
-	modules/gnd_att_control
-	modules/gnd_pos_control
-	modules/mc_att_control
-	modules/mc_pos_control
-	modules/vtol_att_control
+	# CDT12 debug
+	#modules/fw_att_control
+#	modules/fw_pos_control_l1
+#	modules/gnd_att_control
+#	modules/gnd_pos_control
+#	modules/mc_att_control
+#	modules/mc_pos_control
+#	modules/vtol_att_control
 
 	#
 	# Logging
